fe98d89794c7d0c5113c74a6be0a511250387af1
dan_tonye <danthurnye@gmail.com>
2024-04-30 14:07:11 -0400

    added morgan package

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-
-// Defining the data base
-// const db = mongoose.connect(process.env.DATABASE_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
 const db = mongoose.connect('mongodb://localhost/FoodieBlog',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
