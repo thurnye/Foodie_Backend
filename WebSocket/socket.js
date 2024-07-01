353624c0c1c4892b0531dd137c3b3920bee078ba
dan_tonye <danthurnye@gmail.com>
2024-07-01 14:38:20 -0400

    added video call for socket.io

--- WebSocket/socket.js
+++ WebSocket/socket.js
@@ -159,30 +159,41 @@ function initializeSocket(server) {
     // ==========================================================================================
     // Video Chat Offer
 
-    socket.on('join video room', (roomId) => {
-      if (rooms[roomId]) {
-        rooms[roomId].push(socket.id);
-      } else {
-        rooms[roomId] = [socket.id];
-      }
-      const otherUser = rooms[roomId].find((id) => id !== socket.id);
-      if (otherUser) {
-        socket.emit('other user', otherUser);
-        socket.to(otherUser).emit('user joined', socket.id);
-      }
-    });
+    // socket.on('join video room', (roomId) => {
+    //   if (rooms[roomId]) {
+    //     rooms[roomId].push(socket.id);
+    //   } else {
+    //     rooms[roomId] = [socket.id];
+    //   }
+    //   const otherUser = rooms[roomId].find((id) => id !== socket.id);
+    //   if (otherUser) {
+    //     socket.emit('other user', otherUser);
+    //     socket.to(otherUser).emit('user joined', socket.id);
+    //   }
+    // });
 
-    socket.on('offer', (payload) => {
-      io.to(payload.target).emit('offer', payload);
-    });
+    // socket.on('offer', (payload) => {
+    //   io.to(payload.target).emit('offer', payload);
+    // });
 
-    socket.on('answer', (payload) => {
-      io.to(payload.target).emit('answer', payload);
-    });
+    // socket.on('answer', (payload) => {
+    //   io.to(payload.target).emit('answer', payload);
+    // });
 
-    socket.on('ice-candidate', (incoming) => {
-      io.to(incoming.target).emit('ice-candidate', incoming.candidate); // for best connection btwn candidates
-    });
+    // socket.on('ice-candidate', (incoming) => {
+    //   io.to(incoming.target).emit('ice-candidate', incoming.candidate); // for best connection btwn candidates
+    // });
+
+    socket.emit("me", socket.id);
+    
+    socket.on("callUser", (data) => {
+      io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+    })
+  
+    socket.on("answerCall", (data) => {
+      io.to(data.to).emit("callAccepted", data.signal)
+    })
+
 
     // ==========================================================================================
 
